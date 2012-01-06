ba746e2ad6a89d986495556f281069419070dcf3
Alexander Freas <akfreas@gmail.com>
2012-01-05 21:21:27 -0500

    Initial Commit

---
+++ PillOrPokemon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PillOrPokemon
+//
+//  Created by Alexander Freas on 1/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PillOrPokemonTests/PillOrPokemonTests.h
@@ -0,0 +1,13 @@
+//
+//  PillOrPokemonTests.h
+//  PillOrPokemonTests
+//
+//  Created by Alexander Freas on 1/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PillOrPokemonTests : SenTestCase
+
+@end
